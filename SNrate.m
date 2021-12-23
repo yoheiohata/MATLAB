d65e03ff19a5eac274145d467638e72ace1d638f
yohei <8118015@ed.tus.ac.jp>
2021-12-23 22:59:32 +0900

    SN比計算のプログラムを整形した．
    プログラムが汚かったので，他にあるものと構成が同じになるように
    適宜コメントアウトを加えながら，SN比が計算されるようにプログラムを構成し直した．

--- SNrate.m
+++ SNrate.m
@@ -28,152 +28,151 @@ disp('                   HT         |       nCHT')
 disp('------------------------------|-----------------------')
 disp('            normal     noize  | normal    noize')
 disp('==============================|=======================')
-%% フィルタ設計部
+%% params
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%%---------------------フィルタのパラメータ変更部分------------------------
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-%各フィルタの次数部
-N_rearHT=26;
-
-%正規化周波数帯域設定
+% filter params
+N=30;
 f_left_stop  = 0.2;
-f_right_stop = 0.8;
 f_left_pass  = 0.3;
 f_right_pass = 0.7;
-
-
-
-%ノッチの場所
-notch_freq_1 = 0.1;       
-notch_freq_2 = 0.9;
-
-
-
-%重み[左側阻止域　通過域　右側阻止域]
+f_right_stop = 0.8;
+notch_f1 = 0.1;       
+notch_f2 = 0.9;
 Weight = [1 1 1];         
+fs = 50000;
+fs2 = fs/2;
+L=15000;
+ts=1/fs;
+t=(0:L-1)*ts;
+F=0:0.1:fs2;
+f = [0 f_left_stop f_left_pass f_right_pass f_right_stop 1];
+a = [0 0 1 1 0 0];
 
-
-%%--------------------------------------------------------------------
-
-%ヒルベルトフィルタ設計部
-fs = 50000;          %Sampling frequency;
-fs2 = fs/2;          %ナイキスト周波数
-L=15000;             %信号長 15000
-ts=1/fs;             %サンプリング周期
-t=(0:L-1)*ts;        %サンプリング時間
-F=0:0.1:fs2;         %周波数設定
-f = [0 f_left_stop f_left_pass f_right_pass f_right_stop 1]; %振幅特性の通過域、遷移域、阻止域の周波数指定
-a = [0 0 1 1 0 0];         %理想周波数特性[阻止域　通過域　阻止域]
-%振幅特性0、振幅特性1、振幅特性0、
-%繋がってないところは遷移域
-
-
-%% 信号定義  
-
-%振幅定義
+% signal params(signal frequency is below)
 A=1;
 A_noize_rate_1=0.7;
 A_noize_rate_2=0.5;
 
-% インクリメント
+% figure params
 p=0;
-q=0;
-r=0;
-% figureの設定
-y_low = -100;
+y_low = -150;
 y_high = 20;
-%% ノッチヒルベルト用ヒルベルト変換器(手法1と3に用いるやつ)
-%ノッチヒルベルト用ヒルベルト変換器
-[h_BPHT,err,res]=firpm(N_rearHT, f, a, {10000}, Weight,'hilbert'); %フィルタ係数の算出
+
+%% design filter
+
+% conventional
+% bandpass hilbert
+[h_BPHT,err,res]=firpm(N, f, a, {10000}, Weight,'hilbert'); %フィルタ係数の算出
 [H_BPHT,w]=freqz(h_BPHT,1,F,fs);
-%% ノッチフィルタ
 
-%　notch filter
-nf1 = 3600*180/fs2;    r1=1;
-nf2 = 7200*180/fs2;    r2=1;
+
+% proposed
+% notch
+nf1 = 3600*180/fs2;    r1=1;    D2=1;
+nf2 = 7200*180/fs2;    r2=1;    D4=1;
 nf3 = 3600*180/fs2;    r3=1;
-
-D2=1;
-D4=1;
-
 notch_weight1=[1 1 1];
 notch_amplitude1=[0 0 1 1 0 0];
-
 [notch1,notch2,notch3]=notch(nf1,nf2,nf3,D2,D4,fs2,r1,r2,r3);
-notch1 = [1 -2*cos(notch_freq_1*pi) 1*1];
-notch2 = [1 -2*cos(notch_freq_2*pi) 1*1];
+notch1 = [1 -2*cos(notch_f1*pi) 1*1];
+notch2 = [1 -2*cos(notch_f2*pi) 1*1];
 notch3 = conv(notch1,notch2);
-
 notch1 = notch1/sum(notch1);
 notch2 = notch2/sum(notch2);
 notch3 = notch3/sum(notch3);
-
 save notch1.cof notch1 -ascii;
 save notch2.cof notch2 -ascii;
 save notch3.cof notch3 -ascii;
-
 fresp3 = {'fresp_h3',a};
 h_notch = load('notch3.cof');
 [H_notch,w]=freqz(h_notch,1,F,fs);   
 
-%補正ヒルベルトフィルタの作成
-h_coefficient_compensationHT=firpm(N_rearHT,f,fresp3,Weight,'hilbert');
-[h_amplitude_compensationCHT,w]=freqz(h_coefficient_compensationHT,1,F,fs);               %補正のフィルタの振幅特性
+% hilbert
+h_HT=firpm(N-2,f,fresp3,Weight,'hilbert');
+[H_HT,w]=freqz(h_HT,1,F,fs);
 
+% convolution
+h_CHT=conv(h_notch,h_HT);
+[H_CHT,w]=freqz(h_CHT,1,F,fs);
 
-%フィルタ合成(ノッチフィルタ＋ヒルベルト変換器)
-h_coefficient_notchCHT=conv(h_notch,h_coefficient_compensationHT);
-[h_amplitude_notchCHT,w]=freqz(h_coefficient_notchCHT,1,F,fs);
+%% draw filter
+figure;
+x = [notch_f1 notch_f1];
+y = [y_low y_high];
+line(x,y,'Color','green','linestyle','--',"DisplayName","noize="+notch_f1);
+x = [notch_f2 notch_f2];
+y = [y_low y_high];
+line(x,y,'Color','green','linestyle','--',"DisplayName","noize="+notch_f2);
+hold on
+plot(w/fs2, 20*log(abs(H_BPHT)),"DisplayName","BPHT")
+plot(w/fs2, 20*log(abs(H_CHT)),"DisplayName","nCHT")
+hold off
+xlim([0 1]);
+ylim([y_low y_high]);
+xlabel("Normalized Angular Frequency(\times\pi)   [rad/sample]");
+ylabel("Magnitude   [dB] ");
+legend("location", "southwest")
 
-%% 設計したフィルタでヒルベルト変換
+%% filtering & calucurate SNR
 for j=(f_left_pass*fs2):2000:(f_right_pass*fs2)
-    p=p+1;
+p=p+1;
     
-%% 信号定義
-%周波数定義
-sig_f=j;                        %周波数
-normalized_sig_f = sig_f/fs2;   %正規化周波数
-sig_f_noize1=notch_freq_1*fs2;
-sig_f_noize2=notch_freq_2*fs2;
+%% define signals
 
-%正弦波信号
+% define frequency
+sig_f=j;
+normalized_sig_f = sig_f/fs2;
+sig_f_noize1=notch_f1*fs2;
+sig_f_noize2=notch_f2*fs2;
+
+% signals
 sig=A*cos(2*pi*sig_f*t);
-%ノイズあり信号
-
-sig_n=A*cos(2*pi*sig_f*t)+A_noize_rate_1*A*cos(2*pi*sig_f_noize1*t); 
+% sig_n=A*cos(2*pi*sig_f*t)+A_noize_rate_1*A*cos(2*pi*sig_f_noize1*t); 
 % sig_n=A*cos(2*pi*sig_f*t)+A_noize_rate_2*A*cos(2*pi*sig_f_noize2*t);
-% sig_n=A*cos(2*pi*sig_f*t)+A_noize_rate_1*A*cos(2*pi*sig_f_noize1*t)+...
-%       A_noize_rate_2*A*cos(2*pi*sig_f_noize2*t); 
+sig_n=A*cos(2*pi*sig_f*t)+A_noize_rate_1*A*cos(2*pi*sig_f_noize1*t)+...
+      A_noize_rate_2*A*cos(2*pi*sig_f_noize2*t); 
 
-%ゼロ値付加
-sig_1=[sig zeros(1,N_rearHT/2)];
-sig_n_1=[sig_n zeros(1,N_rearHT/2)];
+%% filtering
 
-%% ヒルベルト変換とSN比計算
+sig_1=[sig zeros(1,N/2)];
+sig_n_1=[sig_n zeros(1,N/2)];
 
-%阻止域を有するのFIRヒルベルト変換器とSN比
-sig_htout=filter(h_BPHT,1,sig_1);
-sig_noize_htout=filter(h_BPHT,1,sig_n_1);
-SNR_out=snr(sig_htout);
-SNR_noize_out=snr(sig_noize_htout);
+% filtering BPHT (w/ w/o noize)
+sig_Im_BPHT=filter(h_BPHT,1,sig_1);
+sig_n_Im_BPHT=filter(h_BPHT,1,sig_n_1);
 
-%nCHTとそのSN比
-sig_Im_nCHT=filter(h_coefficient_notchCHT,1,sig_1);
-sig_noize_Im_nCHT=filter(h_coefficient_notchCHT,1,sig_n_1);
-SNR_nCHT_out=snr(sig_Im_nCHT);
-SNR_noize_nCHT_out=snr(sig_noize_Im_nCHT);
+% filtering nCHT (w/ w/o noize)
+sig_Im_nCHT=filter(h_CHT,1,sig_1);
+sig_n_Im_nCHT=filter(h_CHT,1,sig_n_1);
+
+%% calucurate SNR
+
+SNR_BPHT_out=snr(sig_Im_BPHT, fs);
+SNR_noize_BPHT_out=snr(sig_n_Im_BPHT, fs);
+SNR_nCHT_out=snr(sig_Im_nCHT, fs);
+SNR_noize_nCHT_out=snr(sig_n_Im_nCHT, fs);
 
 snr_outcome(p,1) = normalized_sig_f;
-snr_outcome(p,2) = SNR_out;
-snr_outcome(p,3) = SNR_noize_out;
+snr_outcome(p,2) = SNR_BPHT_out;
+snr_outcome(p,3) = SNR_noize_BPHT_out;
 snr_outcome(p,4) = SNR_nCHT_out;
 snr_outcome(p,5) = SNR_noize_nCHT_out;
-%% 入出力描画
+
+% if figures are needed, insert here
+end
+
+%% display SNR outcome
+disp(snr_outcome)
+disp('------------------------------------------------------')
+disp('avarage')
+SNR_outcome(:,2)=mean(snr_outcome(:,2),1);
+SNR_outcome(:,4)=mean(snr_outcome(:,4),1);
+SNR_outcome(:,3)=mean(snr_outcome(:,3),1);
+SNR_outcome(:,5)=mean(snr_outcome(:,5),1);
+disp(SNR_outcome)
+disp('------------------------------------------------------')
+
+%% extras
 % figure;
 % plot(t,sig,"DisplayName","in");
 % hold on;
@@ -190,14 +189,3 @@ snr_outcome(p,5) = SNR_noize_nCHT_out;
 % % exportgraphics(gca,strcat('..\CAS_final\fig\SNR_normalized\f=',num2str(normalized_sig_f),'.pdf'),'ContentType','vector');
 % % exportgraphics(gca,strcat('..\CAS_final\fig\SNR\f=',num2str(sig_f),'.pdf'),'ContentType','vector');
 % % 
-end
-disp(snr_outcome)
-disp('------------------------------------------------------')
-SNR_outcome(:,1)=mean(snr_outcome(:,2),1);
-SNR_outcome(:,2)=mean(snr_outcome(:,4),1);
-SNR_outcome_noize(:,1)=mean(snr_outcome(:,3),1);
-SNR_outcome_noize(:,2)=mean(snr_outcome(:,5),1);
-disp(SNR_outcome)
-disp(SNR_outcome_noize)
-
-
